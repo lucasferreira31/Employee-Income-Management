cbaf4d85ae3447f5633d8e64cc549b73132ad0ff
LUCAS FERREIRA MACHADO <lukaslgmm@gmail.com>
2026-08-17 21:25:56 -0300

    Rename namespace for WorkerLevel enum

--- Entities/Enums/WorkerLevel.cs
+++ Entities/Enums/WorkerLevel.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ProjetoComposiçao.Entities.Enums
+﻿
+namespace EmployeeIncomeManagement.Entities.Enums
 {
      enum WorkerLevel : int
     {
