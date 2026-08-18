ef877dba0fd62ce83d9c2c50e1ceef261ade1b9e
LUCAS FERREIRA MACHADO <lukaslgmm@gmail.com>
2026-08-17 21:24:32 -0300

    Rename namespace from ProjetoComposiçao to EmployeeIncomeManagement

--- Entities/Departament.cs
+++ Entities/Departament.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace ProjetoComposiçao.Entities
+namespace EmployeeIncomeManagement.Entities
 {
     class Departament
     {
